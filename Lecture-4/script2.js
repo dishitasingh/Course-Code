ab82642d6b9ef26e71e8f6858099b84a34bffdbd
Dishita Singh <dishitarajput31@gmail.com>
2020-12-13 10:24:43 +0530

    Adding files to rep

---
+++ Lecture-4/script2.js
@@ -0,0 +1,15 @@
+console.log("ffff");
+function swap(arr, index1, index2){
+    var temp = arr[index1];
+    arr[index1] = arr[index2];
+    arr[index2] = temp;
+}
+
+function insertionSort(arr){
+    let length = arr.length;
+
+    var pointer = arr[i];
+    
+
+    return arr;}
+console.log(insertionSort([76, 45, 58, 23]));
